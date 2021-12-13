772cade42f06b6a1c053f802ea5aca082e2c53d9
Elias Ayele <eliasey11@gmail.com>
2021-12-13 16:17:30 +0300

    image with modifiers and box for stacking items

--- app/src/main/java/com/example/composetut/MainActivity.kt
+++ app/src/main/java/com/example/composetut/MainActivity.kt
@@ -4,54 +4,51 @@ package com.example.composetut
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetut.ui.theme.ComposeTutTheme
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-             Column(
-                 modifier = Modifier
-                     .background(Color.Green)
-                     .fillMaxHeight(0.5f)
-                     .fillMaxWidth()
+        //painter for taking resource
+        //strings for description
+        setContent  {
+            val painter = painterResource(id = R.drawable.sampleimage)
+            val description = "kermit playing in the snow"
+            val title = "Kermit is playing in the snow"
 
-                    // .padding(horizontal = 40.dp, vertical = 50.dp)
-                    // .width(600.dp)
-                    //  .requiredWidth(600.dp)
-                     .border(5.dp, Color.Magenta)
-                     .padding(5.dp)
-                     .border(5.dp,Color.Blue)
-                     .padding(5.dp)
-                     .border(5.dp,Color.Black)
-                     .padding(5.dp)
-
-
-
-
-             ) {
-
-              Text(text = "Hello",
-                  modifier = Modifier.offset(0.dp,20.dp))
-                 Spacer(modifier = Modifier.height(50.dp))
-                 Text(text = "World!", modifier =
-                 Modifier.clickable{
-
-                 })
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+                .clickable {
+                }
+            ) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
+                )
             }
-
         }
     }
 }
@@ -59,14 +56,57 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "$name!")
+fun ImageCard(
+    painter:Painter,
+    contentDescription:String,
+    title:String,
+    modifier:Modifier = Modifier
+) {
+Card(
+    modifier = modifier.fillMaxWidth(),
+    shape = RoundedCornerShape(15.dp),
+    elevation = 5.dp
+    ) {
+    Box(modifier = Modifier.height(200.dp)){
+        Image(
+            painter = painter,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop
+        )
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors  = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 200f,
+
+                )
+            ))
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+            contentAlignment = Alignment.BottomStart
+        ){
+            Text(title,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp
+                )
+            )
+        }
+        
+    }
+
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTutTheme {
-        Greeting("elias")
+        Text("elias")
     }
 }
