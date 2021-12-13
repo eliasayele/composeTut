529cd8646a515056bf403c66ace35a2d22d6f799
Elias Ayele <eliasey11@gmail.com>
2021-12-13 15:04:37 +0300

    different compose modifiers

--- app/src/main/java/com/example/composetut/MainActivity.kt
+++ app/src/main/java/com/example/composetut/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,20 +22,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-             Row(
-                modifier =  Modifier
-                .width(300.dp)
-                .height(300.dp)
-                .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                verticalAlignment = Alignment.CenterVertically,
-                ) {
+             Column(
+                 modifier = Modifier
+                     .background(Color.Green)
+                     .fillMaxHeight(0.5f)
+                     .fillMaxWidth()
 
-                Greeting(name = "elias")
-                Greeting(name = "elias")
-                 
-                Greeting(name = "elias")
+                    // .padding(horizontal = 40.dp, vertical = 50.dp)
+                    // .width(600.dp)
+                    //  .requiredWidth(600.dp)
+                     .border(5.dp, Color.Magenta)
+                     .padding(5.dp)
+                     .border(5.dp,Color.Blue)
+                     .padding(5.dp)
+                     .border(5.dp,Color.Black)
+                     .padding(5.dp)
 
+
+
+
+             ) {
+
+              Text(text = "Hello",
+                  modifier = Modifier.offset(0.dp,20.dp))
+                 Spacer(modifier = Modifier.height(50.dp))
+                 Text(text = "World!", modifier =
+                 Modifier.clickable{
+
+                 })
             }
 
         }
