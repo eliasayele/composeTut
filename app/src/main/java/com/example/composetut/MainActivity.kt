88a56a34c98227a55cfc26eee622d1fc0c5b76b1
Elias Ayele <eliasey11@gmail.com>
2021-12-15 09:35:40 +0300

    constraint layout in compose added

--- app/src/main/java/com/example/composetut/MainActivity.kt
+++ app/src/main/java/com/example/composetut/MainActivity.kt
@@ -22,6 +22,7 @@ import com.example.composetut.ui.theme.ComposeTutTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -33,8 +34,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -49,54 +54,36 @@ class MainActivity : ComponentActivity() {
         //painter for taking resource
         //strings for description
         setContent {
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenbox")
+                val redBox = createRefFor("redbox")
 
-            //get indxed list
-            LazyColumn{
-                itemsIndexed(listOf("this","is","jetpack","Compose"),
-                ){ index,item ->
-                    Text(
-                        text = "$item",
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+                constrain(greenBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                constrain(redBox){
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+                createHorizontalChain(greenBox,redBox)
+
+            }
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.background(Color.Green).layoutId("greenbox")) {
+                    
+                }
+                Box(modifier = Modifier.background(Color.Red).layoutId("redbox")) {
+
                 }
 
             }
-
-            //items loaded render when needed
-//            LazyColumn{
-//                items (5000){
-//                    Text(
-//                        text = "Item $it",
-//                        fontSize = 24.sp,
-//                        fontWeight = FontWeight.Bold,
-//                        textAlign = TextAlign.Center,
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .padding(vertical = 24.dp)
-//                        )
-//                }
-//            }
-
-//            val scrollState = rememberScrollState()
-            //this items loaded during statrup which is bad
-//            Column(modifier = Modifier.verticalScroll(scrollState)) {
-//                for (i in 1..50){
-//                    Text(
-//                        text = "Item $i",
-//                        fontSize = 24.sp,
-//                        fontWeight = FontWeight.Bold,
-//                        textAlign = TextAlign.Center,
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .padding(vertical = 24.dp)
-//                        )
-//                }
-//            }
         }
     }
 }
