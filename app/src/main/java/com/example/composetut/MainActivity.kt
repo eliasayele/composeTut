cd285f326e6ae136bdc81ef3b9762fb2914f7d94
Elias Ayele <eliasey11@gmail.com>
2021-12-20 10:12:09 +0300

    animation tween, keyframes spring and infinite transition

--- app/src/main/java/com/example/composetut/MainActivity.kt
+++ app/src/main/java/com/example/composetut/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.composetut
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
@@ -57,12 +59,49 @@ class MainActivity : ComponentActivity() {
         //painter for taking resource
         //strings for description
         setContent {
-            ComposeTutTheme {
-                Surface(color= MaterialTheme.colors.background) {
-                    ViewPagerSlider()
-
-                }
+            var sizeState by remember {
+                mutableStateOf(200.dp)
             }
+            val size by animateDpAsState(
+                targetValue = sizeState ,
+                tween(
+                    durationMillis = 1000
+                )
+//                keyframes {
+//                    durationMillis= 5000
+//                    sizeState at 0 with LinearEasing
+//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//                    sizeState *2f at 5000
+//                }
+//                spring(
+//                    Spring.DampingRatioHighBouncy
+//                )
+//                    tween(
+//                        durationMillis = 3000,
+//                        delayMillis = 300,
+//                        easing = LinearOutSlowInEasing
+//                    )
+                )
+            val infiniteTransition  = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red ,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+             Box(modifier = Modifier
+                 .size(size)
+                 .background(color),
+                 contentAlignment = Alignment.Center
+             ){
+                 Button(onClick = {
+                     sizeState += 50.dp
+                 }) {
+                     Text("Increase Size")
+                 }
+             }
         }
     }
 }
