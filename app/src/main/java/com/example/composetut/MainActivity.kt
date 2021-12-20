cfd3b892b75d835e80b59e9a6636baac2bbb2e03
Elias Ayele <eliasey11@gmail.com>
2021-12-20 21:24:38 +0300

    custom animation for circular progress bar

--- app/src/main/java/com/example/composetut/MainActivity.kt
+++ app/src/main/java/com/example/composetut/MainActivity.kt
@@ -4,50 +4,32 @@ package com.example.composetut
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.composetut.ui.theme.ComposeTutTheme
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.*
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import com.example.composetut.view.ViewPagerSlider
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composetut.ui.theme.ComposeTutTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
-import kotlinx.coroutines.Delay
-import kotlinx.coroutines.launch
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,43 +38,70 @@ class MainActivity : ComponentActivity() {
             Font(R.font.lexenddeca_thin, FontWeight.Thin),
             Font(R.font.lexenddeca_light, FontWeight.Light),
         )
-        //painter for taking resource
-        //strings for description
-        setContent {
 
+        setContent {
+        Box (
+            contentAlignment = Alignment.Center,
+             modifier = Modifier.fillMaxSize()
+        )
+        {
+                CircularProgressBar(percentage = 0.8f, number = 100)
         }
+     }
     }
 }
 
-
 @Composable
 fun CircularProgressBar(
-    percentage:Float,
-    number:Int,
-    fontSize:TextUnit,
-    radius: Dp = 28.dp,
-    color:Color = Color.Green,
-    strokeWidth:Dp = 8.dp,
-    animDuration:Int = 1000,
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
     animDelay: Int = 0
-)
-{
+) {
     var animationPlayed by remember {
         mutableStateOf(false)
     }
+
     val curPercentage = animateFloatAsState(
-        targetValue = if(animationPlayed)
+        targetValue = if (animationPlayed)
             percentage else 0f,
         animationSpec = tween(
             durationMillis = animDuration,
             delayMillis = animDelay
         )
-        )
+    )
 
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round),
+                )
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+    }
 
 
 }
-
 
 
 @Preview(showBackground = true)
